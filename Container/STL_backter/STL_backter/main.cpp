0deaab3fbd93dffdc0e5e158093fb640af754e30
Shin_Bo_Won <bowo9777@naver.com>
2022-06-10 11:42:32 +0900

    2022-06-10 Vector ver0.2

--- Container/STL_backter/STL_backter/main.cpp
+++ Container/STL_backter/STL_backter/main.cpp
@@ -7,13 +7,19 @@ int main(void)
 {
 	vector<int> vecNumbers; // 벡터 형 int
 
-	for (int i = 0; i < 10 ; ++i)
-		vecNumbers.push_back(10 *i );
+	for (int i = 0; i < 10; ++i)
+		vecNumbers.push_back(10 * i);
 
-	//vector<int> 네임스페이스 iterator 하나의 형태 
+	// **vector<int> 네임스페이스 iterator 하나의 형태 
+	// **직접 접근이 어려운 경우 반복자를 사용하여 대신 접근 
+	/*
 	for (vector<int>::iterator iter = vecNumbers.begin(); iter != vecNumbers.end(); ++iter)
 		cout << (*iter) << endl;
-
+	for (int i = 0; i < vecNumbers.size(); ++i)
+		cout << vecNumbers[i] << endl;
+	for (auto iter = vecNumbers.begin(); iter != vecNumbers.end(); iter++)
+		cout << (*iter) << endl;
+	*/
 	/*
 	int Array[16] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };	
 
@@ -25,8 +31,38 @@ int main(void)
 	for (int i = 0; i < 16; ++i)
 		cout << *(iterator + i) << endl;
 	*/
+	/*
+	for (auto iter = vecNumbers.begin(); iter != vecNumbers.end(); )
+	{
+		
+		if ((*iter) > 50) // 충돌 판정
+			// 만약 포문에서 무슨 이상 값 을 지울려면
+			// erase 는 자신의 값을 지우고 자신에게 값을 대입 할것
+			iter = vecNumbers.erase(iter);
+		else
+			++iter;
+	}
+
+	for (auto iter = vecNumbers.begin(); iter != vecNumbers.end();++iter )
+		cout << (*iter) << endl;
+	*/
+	/*
+	// ** 가장 앞에 있는 [원소를 반환]
+	vecNumbers.front();
+
+	// ** 가장 뒤에 있는 [ 원소를 반환]
+	vecNumbers.back();
+
+	// ** 시작점을 가르키는 [ 반복자]
+	vecNumbers.begin();
+
+	// ** 시작점을 가르키는 [ 반복자]
+	vecNumbers.end();
+	*/
+
 	return 0;
 }
+
 // 언어를 쓰는 이유 데인터 관리-> 포인터 사용을 위한 이유 
 /*
 	int* Pointer = new int ; // new int 가 heap 영역에 있고
